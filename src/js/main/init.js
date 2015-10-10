5330e9eb0edc0053765a0a4f2cc2b6ea2541e881
state-hiu-dev <HIU_DEV@state.gov>
2015-10-10 12:56:42 -0400

    working on angular

---
+++ src/js/main/init.js
@@ -0,0 +1,26 @@
+var init_main = function(appName, prefixFilter)
+{
+    var mainApp = angular.module(appName, []);
+    $('.site-controller').each(function({
+        var that = $(this);
+        var controllerName = that.data('siteControllerName');
+        var controllerType = that.data('siteControllerType');
+        if(controllerType.toLowerCase()=="data")
+        {
+            var url = that.data('siteControllerUrl');
+            init_controller_data(app, that, controllerName, prefixFilter, url);
+        }
+    });
+    angular.bootstrap(document, [appName]);
+};
+
+var init_controller_data = function(app, element, controllerName, prefixFilter, url)
+{
+    app.controller(controllerName, function($scope, $http) {
+        $http.get(url)
+            .success(function(response)
+            {
+                $scope.datasets = parseCSW(response, prefixFilter);
+            });
+    });
+};
--- src/js/main/utility.js
+++ src/js/main/utility.js
@@ -34,4 +34,49 @@ var updateRenderOrder = function(layers)
     {
         layers[i].bringToFront();
     }
-}
+};
+
+var formatDate = function(d)
+{
+    if(d!=undefined)
+    {
+        return d.getFullYear()+"-"+((d.getMonth()<9)?("0"+(d.getMonth()+1)):(d.getMonth()+1))+"-"+((d.getDate()<10)?("0"+d.getDate()):d.getDate());
+    }
+    else
+    {
+        return "";
+    }
+};
+
+var parseCSW = function(xml, prefixFilter)
+{
+    var layers = []
+    $(xml).find("SearchResults").find('MD_Metadata').each(function(){
+        vart that = $(this);
+        var i = that.find("identificationInfo").find("MD_DataIdentification");
+        var distro = that.find("distributionInfo").find("MD_Distribution");
+        /////////////
+        var b = i.find('EX_GeographicBoundingBox:first');
+        var url_detail = distro.find('onLine').filter(function(){return that.find('protocol').find('CharacterString').text()=="WWW:LINK-1.0-http--link"}).find('URL').text().trim();
+        /////////////
+        if(url_detail.startsWith(prefixFilter) && b.length > 0)
+        {
+            var title = i.find('title').find('CharacterString').text();
+            var date_published = i.find('date').find('DateTime').text();
+            var url_thumbnail_200x150 = i.find('graphicOverview').find('fileName').text().trim();
+            var abstract = ellipsis(i.find('abstract').find('CharacterString').text().replace('\n',''), 100);
+            var layer = {
+                "title": title;
+                "date_published": date_published;
+                "region":"",
+                "abstract": abstract,
+                "url_detail": url_detail,
+                "url_region": "",
+                "url_thumbnail_200x150": url_thumbnail_200x150
+            };
+            layers.push(layer);
+        }
+    });
+    return layers;
+};
+
