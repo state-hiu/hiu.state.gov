94b6d433a96a63596fdfa1a7e625208bc6987223
d3netxer <tommygun320@yahoo.com>
2017-03-26 21:26:48 -0400

    finished with new gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,104 +1,22 @@
 var gulp = require('gulp');
-var pkg = require('./package.json');
-var concat = require('gulp-concat');
-var minify = require('gulp-minify');
-var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
-var jshint = require('gulp-jshint');
-var less = require('gulp-less');
-var del = require('del');
-var path = require('path');
-var spawn = require('child_process').spawn;
+var rename = require('gulp-rename');
 
-var compilelist =
-[
-    {
-        "name": "main_js",
-        "type": "js",
-        "src": "./src/js/main/*.js",
-        "outfile":"main.js",
-        "dest":"./build/js/"
-    }
-];
+gulp.task('default', function() {
 
-var copylist =
-[
-];
+    gulp.src('lib/app.js')
+      .pipe(uglify())
+      .pipe(rename({ extname: '.min.js'}))
+      .pipe(gulp.dest('build/js'));
 
-gulp.task('compile', function(){
-    for(var i = 0; i < compilelist.length; i++)
-    {
-        var t = compilelist[i];
-        process.stdout.write(t.name);
-        process.stdout.write("\n");
-        if(t.type=="js")
-        {
-            gulp.src(t.src)
-                .pipe(concat(t.outfile))
-                .pipe(gulp.dest(t.dest))
-                .pipe(uglify())
-                .pipe(rename({ extname: '.min.js'}))
-                .pipe(gulp.dest(t.dest));
-        }
-        else if(t.type=="css")
-        {
-            gulp.src(t.src)
-                .pipe(concat(t.outfile))
-                .pipe(gulp.dest(t.dest));
-        }
-    }
-});
+    gulp.src('lib/leaflet-markercluster/leaflet.markercluster.js')
+      .pipe(uglify())
+      .pipe(rename({ extname: '.min.js'}))
+      .pipe(gulp.dest('build/js'));
 
-gulp.task('copy', function(){
-    for(var i = 0; i < copylist.length; i++)
-    {
-        var t = copylist[i];
-        gulp.src(t.src).pipe(gulp.dest(t.dest));
-    }
-});
+    gulp.src('lib/leaflet-list-markers/dist/leaflet-list-markers.src.js')
+      .pipe(uglify())
+      .pipe(rename({ extname: '.min.js'}))
+      .pipe(gulp.dest('build/js'));
 
-gulp.task('clean', function () {
-  return del([
-    './temp/**/*',
-    './build/js/**/*',
-    './build/css/**/*'
-  ]);
-});
-
-gulp.task('test', function(){
-    var scripts =
-    [
-        "./src/js/main/*.js",
-        "./src/js/polyfill/*.js"
-    ];
-    for(var i = 0; i < scripts.length; i++)
-    {
-        gulp.src(scripts[i])
-            .pipe(jshint()).
-            pipe(jshint.reporter('default'));
-    }
-});
-
-gulp.task('default', ['clean', 'copy','compile']);
-
-
-gulp.task('bootstrap:clean', function() {
-    return del([
-        './temp/**/*',
-        './build/bootstrap/**/*'
-    ]);
-});
-gulp.task('bootstrap:prepareLess', ['bootstrap:clean'], function() {
-    var base = "./lib/bootstrap/3.3.5/less/";
-    return gulp.src([base+'/**', '!'+base+'/{variables.less}'])
-        .pipe(gulp.dest('./temp'));
-});
-gulp.task('bootstrap:prepareVariables', ['bootstrap:prepareLess'], function() {
-    return gulp.src('./src/less/bootstrap/variables.less')
-        .pipe(gulp.dest('./temp'));
-});
-gulp.task('bootstrap:compile', ['bootstrap:prepareVariables'], function() {
-    return gulp.src('./temp/bootstrap.less')
-        .pipe(less())
-        .pipe(gulp.dest('./build/bootstrap'));
 });
